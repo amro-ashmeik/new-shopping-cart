914b2faee6c040fd135ca6db33e2391b72e06092
Amro Ashmeik <amroashmeik2020@u.northwestern.edu>
2020-04-11 05:42:54 -0700

    firebase setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB6QGVdcfJowjDNyo6EfMpGnIinu9nQv40",
+  authDomain: "new-shopping-cart-4563a.firebaseapp.com",
+  databaseURL: "https://new-shopping-cart-4563a.firebaseio.com",
+  projectId: "new-shopping-cart-4563a",
+  storageBucket: "new-shopping-cart-4563a.appspot.com",
+  messagingSenderId: "928605069032",
+  appId: "1:928605069032:web:df935ef0a05436dd28335d",
+  measurementId: "G-XGE4X92EMQ"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 function App() {
   return (
     <div className="App">
